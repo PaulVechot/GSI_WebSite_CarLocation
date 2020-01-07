93aa4820681aa2e6b74d9054156b23b93aba6ccb
FouratHebiri <59573804+FouratHebiri@users.noreply.github.com>
2020-01-07 17:28:57 +0100

    Conf.php

--- Controler/ConnexionBDD.php
+++ Controler/ConnexionBDD.php
@@ -1,10 +1,12 @@
 <?php
+
+	// Appel fichier conf.php
+	INCLUDE 'conf.php';
+
 	//ConnexionBDD
 	$c = new mysqli ("localhost" , "root" , "", "gestion", 8888);
 		if ($c->connect_errno) {
 		die ("Erreur de connexion (".$c->connect_errno."): ".$c-> connect_error);
 	}
-	$Y= "localhost";
 
-	// Creation fichier conf.php dans controler dedans je créer des variables avec des noms parlants (je cherche sur google mysqli ("localhost" , "root" , "", "gestion", 8888) pour voir à quoi ça correspond) et puis je fais appel au fichier créee (conf.php) dans ce fichier la (connexionBDD)
 ?>
---
+++ Controler/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+	$host= "localhost";
+	$username= "root";
+	$password= "";
+	$bdd= "gestion";
+	$port= 8888;
+	
+?>
